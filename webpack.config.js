d90e86e9878fb479756d14a328cbb18568599b68
David Fonollosa <david.fonollosa@atrapalo.com>
2020-04-09 16:28:33 +0200

    add initial config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,68 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  title: 'AR React Fun Fun Fun',
-  template: 'templates/index.html'
+    title: 'AR React Fun Fun Fun',
+    template: 'templates/index.html',
 });
 
 const cleanWebpackPlugin = new CleanWebpackPlugin({
-  verbose: true
-})
+    verbose: true,
+});
 
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[name]_[id].css',
+});
 
 module.exports = {
-    mode: "none",
-    entry: "./src/index",
+    mode: 'none',
+    entry: './src/index',
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-      publicPath: "/",
-      library: "ArReactFun",
-      libraryTarget: "umd"
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+        publicPath: '/',
+        library: 'ArReactFun',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules'],
+                            },
+                            // Prefer Dart Sass
+                            implementation: require('sass'),
+
+                            // See https://github.com/webpack-contrib/sass-loader/issues/804
+                            webpackImporter: false,
+                        },
+                    },
+                ],
+            },
+        ],
     },
     devtool: 'eval-cheap-module-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      clientLogLevel: 'debug'
+        contentBase: path.join(__dirname, 'dist'),
+        clientLogLevel: 'debug',
     },
-    plugins: [cleanWebpackPlugin, htmlWebpackPlugin]
-}
+    plugins: [cleanWebpackPlugin, htmlWebpackPlugin, miniCssExtractPlugin],
+};
