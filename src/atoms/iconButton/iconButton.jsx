ba68ce86c7db4eaf03ce0b04a72ffd825721ebfa
David Fonollosa <david.fonollosa@atrapalo.com>
2020-04-15 17:15:25 +0200

    allow extra classnames

--- src/atoms/iconButton/iconButton.jsx
+++ src/atoms/iconButton/iconButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './iconButton.scss';
 
 function IconButton(props) {
-    const { type } = props;
+    const { type, className } = props;
     return (
-        <button type="button" className="mdc-icon-button material-icons">
+        <button type="button" className={`mdc-icon-button material-icons ${className}`}>
             {type}
         </button>
     );
