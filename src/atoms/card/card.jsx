8c4b8f27978cc5b8e5f7fd910f0559d2381d3918
David Fonollosa <david.fonollosa@atrapalo.com>
2020-04-15 17:17:34 +0200

    card component

--- src/atoms/card/card.jsx
+++ src/atoms/card/card.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
+import PrimaryAction from './primaryAction';
+import RichMedia from './richMedia';
+import { Headline6, Subtitle2 } from '../typography';
+import Actions, { ActionButtons, ActionIcons } from './actions';
 import './card.scss';
 
+const createActions = (buttons, icons) => {
+    if (buttons || icons) {
+        return (
+            <Actions>
+                {buttons && <ActionButtons>{buttons}</ActionButtons>}
+                {icons && <ActionIcons>{icons}</ActionIcons>}
+            </Actions>
+        );
+    }
+
+    return null;
+};
 
 function Card(props) {
-    const { children } = props;
+    const {
+        title, subtitle, media, buttons, icons, description,
+    } = props;
+
     return (
         <div className="mdc-card">
-            {children}
-            {/* <div className="mdc-card__primary-action">
-                <div className="mdc-card__media" style={{ 'background-image': 'url(&quot;https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg&quot;)' }} />
-                <div className="demo-card__primary">
-                    <h2 className="demo-card__title mdc-typography mdc-typography--headline6">Our Changing Planet</h2>
-                    <h3 className="demo-card__subtitle mdc-typography mdc-typography--subtitle2">by Kurt Wagner</h3>
-                </div>
-                <div className="demo-card__secondary mdc-typography mdc-typography--body2">Visit ten places on our planet that are undergoing the biggest changes today.</div>
-            </div>
-            <div className="mdc-card__actions">
-                <div className="mdc-card__action-buttons">
-                    <button type="button" className="mdc-button mdc-card__action mdc-card__action--button">  <span className="mdc-button__ripple" /> Read</button>
-                    <button type="button" className="mdc-button mdc-card__action mdc-card__action--button">  <span className="mdc-button__ripple" /> Bookmark</button>
-                </div>
-                <div className="mdc-card__action-icons">
-                    <button type="button" className="mdc-icon-button mdc-card__action mdc-card__action--icon--unbounded" aria-pressed="false" aria-label="Add to favorites" title="Add to favorites">
-                        <i className="material-icons mdc-icon-button__icon mdc-icon-button__icon--on">favorite</i>
-                        <i className="material-icons mdc-icon-button__icon">favorite_border</i>
-                    </button>
-                    <IconButton type="share" />
-                    <IconButton type="more_vert" />
-                </div>
-            </div> */}
+            <PrimaryAction>
+                {media && <RichMedia media={media} />}
+                {title && <Headline6><h2 className="rfs-card__primary-action__title">{title}</h2></Headline6>}
+                {subtitle && <Subtitle2><h3 className="rfs-card__primary-action__subtitle">{subtitle}</h3></Subtitle2>}
+                {description && <div className="rfs-card__primary-action__description">{description}</div>}
+            </PrimaryAction>
+            {createActions(buttons, icons)}
         </div>
     );
 }
--- src/atoms/card/richMedia.jsx
+++ src/atoms/card/richMedia.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function RichMedia(props) {
-    const { children, title } = props;
+    const { children, title, media = 'https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg' } = props;
 
     return (
-        <div className="my-card__media mdc-card__media mdc-card__media--16-9">
+        <div
+            className="rfs-card__media mdc-card__media mdc-card__media--16-9"
+            style={{ backgroundImage: `url(${media})` }}
+        >
             {title && <div className="mdc-card__media-content">title</div>}
             {children}
         </div>
