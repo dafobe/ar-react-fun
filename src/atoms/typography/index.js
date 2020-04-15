d9f7d03fa4ea6eeaa5546ee9c62e611bb39bdba8
David Fonollosa <david.fonollosa@atrapalo.com>
2020-04-15 17:18:09 +0200

    add supported typography types

--- src/atoms/typography/index.js
+++ src/atoms/typography/index.js
@@ -1,1 +1,13 @@
-export { default } from './typography';
+export {
+    default,
+    headline1 as Headline1,
+    headline2 as Headline2,
+    headline3 as Headline3,
+    headline4 as Headline4,
+    headline5 as Headline5,
+    headline6 as Headline6,
+    subtitle1 as Subtitle1,
+    subtitle2 as Subtitle2,
+    body1 as Body1,
+    body2 as Body2,
+} from './typography';
--- src/atoms/typography/typography.jsx
+++ src/atoms/typography/typography.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
 import './typography.scss';
 
-function Typography() {
-    return <div />;
+const typographyCreator = (type) => (component) => React.cloneElement(component, { className: `${component.props.className || ''} ${type}` });
+const addHeadline = (level) => typographyCreator(`mdc-typography--headline${level}`);
+const addSubtitle = (level) => typographyCreator(`mdc-typography--subtitle${level}`);
+const addBody = (level) => typographyCreator(`mdc-typography--body${level}`);
+
+
+const addTypography = typographyCreator('mdc-typography');
+
+const createStyle = (level, creator) => (props) => {
+    const { children } = props;
+
+    return React.Children.map(children, (child) => creator(`${level}`)(child));
+};
+
+export const headline1 = createStyle('1', addHeadline);
+export const headline2 = createStyle('2', addHeadline);
+export const headline3 = createStyle('3', addHeadline);
+export const headline4 = createStyle('4', addHeadline);
+export const headline5 = createStyle('5', addHeadline);
+export const headline6 = createStyle('6', addHeadline);
+export const subtitle1 = createStyle('1', addSubtitle);
+export const subtitle2 = createStyle('2', addSubtitle);
+export const body1 = createStyle('1', addBody);
+export const body2 = createStyle('2', addBody);
+
+function Typography(props) {
+    const { children } = props;
+
+    return React.Children.map(children, (child) => addTypography(child));
 }
 
 export default Typography;
