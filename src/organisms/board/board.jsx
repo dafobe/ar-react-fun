49384b855f4e17cbef1b297793c85f2b67bfea9f
David Fonollosa <david.fonollosa@atrapalo.com>
2020-05-17 12:43:42 +0200

    simplify scripts loading

--- src/organisms/board/board.jsx
+++ src/organisms/board/board.jsx
@@ -18,36 +18,11 @@ const loadLibrary = (url, onLoad) => {
 
 function Board() {
     const [count, setCount] = useState(0);
-    const [arScriptUrl, setArScriptUrl] = useState('');
     const [arJsLoaded, setArJsLoaded] = useState(false);
     const onIncrementHandler = getCounterIncrementerHandler(setCount, count);
     const onResetHandler = () => setCount(0);
 
-
-    useEffect(() => {
-        if( !arJsLoaded ){
-            const aframe = import(
-                /* webpackChunkName: "aframe" */
-                /* webpackMode: "lazy" */
-                'aframe').then(() => {
-                    console.log('aframe loaded');
-                    setArScriptUrl('https://raw.githack.com/AR-js-org/AR.js/3.0.0/aframe/build/aframe-ar.js');
-                });
-            }
-        }, []);
-
-    useScript(arScriptUrl, () => setArJsLoaded(true));
-
-
-    // useEffect(() => {
-    //     // const aframe = loadLibrary('https://aframe.io/releases/1.0.0/aframe.js');
-    //     const arjs = loadLibrary('https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js', () => setArJsLoaded(true));
-
-    //     return () => {
-    //         document.querySelector('head').removeChild(aframe);
-    //         document.querySelector('head').removeChild(arjs);
-    //     };
-    // }, []);
+    useScript('https://raw.githack.com/AR-js-org/AR.js/3.0.0/aframe/build/aframe-ar.js', () => setArJsLoaded(true));
 
     return (
         <BoardView
